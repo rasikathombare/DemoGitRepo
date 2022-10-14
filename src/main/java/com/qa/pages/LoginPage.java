b9c6d8f2c48521a7bba197443dc55a8c7fcd7834
rasika <rasikast5@gmail.com>
2022-10-14 14:35:52 +0530

    updated login page class

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,7 +1,16 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
 
+	//OR
+	@FindBy(name = "email")
+	WebElement username;
+	@FindBy(name="password")
+	WebElement pswd;
+	
 	public void verifylogin() {
 		
 		System.out.println("login");
